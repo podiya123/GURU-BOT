299b65c1fd02756dfb5ad1448391914844760635
podiya123 <147955710+podiya123@users.noreply.github.com>
2023-10-14 19:36:17 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94789424380', 'me', true],
+  ['94789424380', 'Hashen', true], 
   [''] 
 ] //Number of owners
 
